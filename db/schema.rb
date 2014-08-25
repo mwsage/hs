0f3475c6a4511c400c5aea55da0f4e3d6fc2c137
mike sage <mwsage@gmail.com>
2014-08-24 17:07:03 -0700

    added group model and groups_and_users model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140820060631) do
+ActiveRecord::Schema.define(version: 20140825000525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,22 @@ ActiveRecord::Schema.define(version: 20140820060631) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "groups", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "groups_students", id: false, force: true do |t|
+    t.integer "group_id"
+    t.integer "student_id"
+  end
+
+  create_table "groups_users", id: false, force: true do |t|
+    t.integer "group_id"
+    t.integer "user_id"
+  end
 
   create_table "installs", force: true do |t|
     t.string   "email",                  default: "", null: false
